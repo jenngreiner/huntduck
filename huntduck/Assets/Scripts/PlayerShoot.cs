d854e98747b5387fc7c9a0a707463cf306c5d984
Ben Barone <benjamin.barone1@gmail.com>
2021-08-04 21:55:40 -0400

    Added sweet orange duck and ability to shoot it with Switch statement! in PlayerShoot!

--- huntduck/Assets/Scripts/PlayerShoot.cs
+++ huntduck/Assets/Scripts/PlayerShoot.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 // public class PlayeShoot : NetworkBehavior
 public class PlayerShoot : MonoBehaviour
 {
+    private const string DUCK_TAG = "Duck";
+
     public PlayerWeapon weapon;
 
     // Variable stores camera so ray shoots from center of camera
@@ -47,8 +49,25 @@ public class PlayerShoot : MonoBehaviour
         RaycastHit _hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
         {
-            // We hit something
-            Debug.Log("We hit " + _hit.collider.name);
+            switch (_hit.collider.tag)
+            {
+                case DUCK_TAG:
+                    shotADuck(_hit.collider.name);
+                    break;
+                //case "Shootable":
+                //    Debug.Log("Shot something shootable!");
+                //    break;
+                default:
+                    Debug.Log("missed, stupid idiot! you hit " + _hit.collider.name);
+                    break;
+            }
+
         }
+    }
+
+    void shotADuck(string duckName)
+    {
+        // We hit something shootable
+        Debug.Log("We hit " + duckName);
     }
 }
