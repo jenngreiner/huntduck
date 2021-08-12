6ef0e719a6962811963030b4c6c2171fd6ff1bd2
Jenn Greiner <jenn.greiner1@gmail.com>
2021-08-12 14:01:50 -0600

    Input field to enter and store playername

---
+++ huntduck/Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Com.HuntDuck
+{
+    /// <summary>
+    /// Player name input field. Let the user input their name, will appear above the player in the game.
+    /// </summary>
+    ///
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        #region Private Constants
+
+        // Store PlayerPref Key to avoid typos
+        const string playerNamePrefKey = "PlayerName";
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        ///<summary>
+        /// MonoBehavior method called on GameObject by unity during initialization phase
+        /// </summary>
+        ///
+        private void Start()
+        {
+            string defaultName = string.Empty;
+            InputField _inputField = this.GetComponent<InputField>();
+            if (_inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+
+            PhotonNetwork.NickName = defaultName;
+
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Sets the name of the player, and save it in the PlayerPrefs for future sessions.
+        /// </summary>
+        /// <param name="value">The name f the Player</param>
+        ///
+        public void SetPlayerName(string value)
+        {
+            // #Important
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("Player Nae is null or empty");
+                return;
+            }
+            PhotonNetwork.NickName = value;
+
+
+            PlayerPrefs.SetString(playerNamePrefKey
+                , value);
+        }
+
+        #endregion
+    }
+}
