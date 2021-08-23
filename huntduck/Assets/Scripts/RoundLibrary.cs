6c7a54d087950122b0588af33b58368361e87f22
Jenn Greiner <jenn.greiner1@gmail.com>
2021-08-23 15:11:30 -0600

    Round logic pseudocode

---
+++ huntduck/Assets/Scripts/RoundLibrary.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundLibrary : MonoBehaviour
+{
+
+    //    public enum Rounds =
+    //    [
+    //        "Round1": {
+    //            "Ducks": [
+    //                "Duck1": { "points": 100, "size": 50%},
+    //                "Duck2": { "points": 100, "size": 50%},
+    //                "Duck3": { "points": 100, "size": 50%},
+    //                ],
+    //            "Duration": 200sec,
+    //            },
+    //       "Round2": {
+    //            "Ducks": [
+    //              "Duck1": { "points": 500, "size": 35%},
+    //              "Duck2": { "points": 500, "size": 35%},
+    //              "Duck3": { "points": 500, "size": 35%},],
+    //            "Duration": 100sec,
+    //        },
+    //    ];
+
+    //public static int CurrentRound;
+
+    //startRound() {
+    //     if (Round[i].Ducks.Length > 0 || Round[i]Duration > 0)
+    //    {
+    //        // play game stuff
+    //        // shoot ducks at increment
+    //        // add to score
+    //        // kill ducks when shot
+    //    } else { endRound() }
+
+    //}
+
+    //endRound()
+    //    {
+    //    if (CurrentRounds < TotalRounds)
+    //    {
+    //        CurrentRound++;
+    //    }
+
+    //    if (CurrentRounds >= TotalRounds)
+    //    {
+    //        endGame()
+    //    }
+    //}
+
+    private static int TotalRounds = Rounds.Length;
+
+
+    //for (i = 0; i < Rounds.Length; i++)
+    //{
+    //    Rounds.[i]++;
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+
