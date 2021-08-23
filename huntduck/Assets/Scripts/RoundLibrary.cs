4b6937b3c5a6f0cdbaf558d76afa26e815afc579
Jenn Greiner <jenn.greiner1@gmail.com>
2021-08-23 15:46:39 -0600

    Separate Duck Library

--- huntduck/Assets/Scripts/RoundLibrary.cs
+++ huntduck/Assets/Scripts/RoundLibrary.cs
@@ -8,36 +8,39 @@ public class RoundLibrary : MonoBehaviour
     //    public enum Rounds =
     //    [
     //        "Round1": {
-    //            "Ducks": [
-    //                "Duck1": { "points": 100, "size": 50%},
-    //                "Duck2": { "points": 100, "size": 50%},
-    //                "Duck3": { "points": 100, "size": 50%},
-    //                ],
-    //            "Duration": 200sec,
+    //                Ducks[1], Ducks[2], Ducks[3]
     //            },
     //       "Round2": {
     //            "Ducks": [
-    //              "Duck1": { "points": 500, "size": 35%},
-    //              "Duck2": { "points": 500, "size": 35%},
-    //              "Duck3": { "points": 500, "size": 35%},],
-    //            "Duration": 100sec,
+    //              "Duck1": { "points": 500, "size": 35%, "duration": 30},
+    //              "Duck2": { "points": 500, "size": 35%, "duration": 30},
+    //              "Duck3": { "points": 500, "size": 35%, "duration": 30},],
     //        },
     //    ];
 
+
+    //    public enum Ducks =
+    //    [
+    //        1: { "points": 100, "size": 50%, "duration": 30},
+    //        2: { "points": 200, "size": 50%, "duration": 30},
+    //        3: { "points": 200, "size": 50%, "duration": 25},
+    //        4: { "points": 500, "size": 35%, "duration": 30},
+    //        5: { "points": 600, "size": 35%, "duration": 25},
+    //        6: { "points": 700, "size": 35%, "duration": 20}
+    //    ];
+
+
     //public static int CurrentRound;
 
-    //startRound() {
-    //     if (Round[i].Ducks.Length > 0 || Round[i]Duration > 0)
-    //    {
-    //        // play game stuff
-    //        // shoot ducks at increment
-    //        // add to score
-    //        // kill ducks when shot
-    //    } else { endRound() }
+    //endRound() {
+    //     if (Round[i].Ducks.Length <= 0)
+    //     {
+    //      nextRound()
+    //     } 
 
     //}
 
-    //endRound()
+    //nextRound()
     //    {
     //    if (CurrentRounds < TotalRounds)
     //    {
