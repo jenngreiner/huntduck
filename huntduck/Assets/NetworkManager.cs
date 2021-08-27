087855ad6ca9a391ae25f9978325bf33d914bcc4
Ben Barone <benjamin.barone1@gmail.com>
2021-08-27 17:57:29 -0400

    Reimported photo folder

---
+++ huntduck/Assets/NetworkManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+// connect to the network on first load
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectedToServer();
+    }
+
+    private void ConnectedToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("try connect to server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room");
+        base.OnJoinedRoom();
+    }
+}
