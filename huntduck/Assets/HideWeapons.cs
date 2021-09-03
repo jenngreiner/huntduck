c56f4d68f32e10cd7b313fa091b643a7e774781a
Ben Barone <benjamin.barone1@gmail.com>
2021-09-03 14:23:54 -0400

    fixed sizes of duck, grass, working on wallet scoreboard

---
+++ huntduck/Assets/HideWeapons.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class HideWeapons : MonoBehaviour
+{
+    private void Update()
+    {
+        // if weapon is picked up, hide the whole weapon wall
+        OnWeaponPickedUp += hideWeaponWall();
+    }
+
+    private void hideWeaponWall()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ huntduck/Assets/WeaponPickedUp.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class WeaponPickedUp : MonoBehaviour
+{
+    public delegate void WeaponPickedUpD();
+    public static event WeaponPickedUpD onWeaponPickedUp;
+
+    
+    void On()
+    {
+        if (BNG.SnapZone.)
+    }
+}
