63de1da9b8f4aa7a9a5373dab9e38483b85c6479
Ben Barone <benjamin.barone1@gmail.com>
2021-09-28 17:43:36 -0400

    Added next button for level change (needs testing)

---
+++ huntduck/Assets/NextLevel.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public string nextLevel;
+
+    public void goToLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
