6fde30b8af94e17c196499ed1e2c2710126ac885
MattyQ85 <mattqdesigns@gmail.com>
2021-09-28 17:04:22 -0600

    NextLevel buttons go to infinite wave on destroy

--- huntduck/Assets/NextLevel.cs
+++ huntduck/Assets/NextLevel.cs
@@ -5,6 +5,12 @@ public class NextLevel : MonoBehaviour
 {
     public string nextLevel;
 
+public void Update(){
+    if (Input.GetKeyDown(KeyCode.L)){
+
+        SceneManager.LoadScene(nextLevel);
+    }
+}
     public void goToLevel()
     {
         SceneManager.LoadScene(nextLevel);
