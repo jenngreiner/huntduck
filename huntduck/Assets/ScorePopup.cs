aac4e8d092366e4e20783e419b4afad994ec6169
Ben Barone <benjamin.barone1@gmail.com>
2021-12-29 23:15:36 -0500

    added effectType dropdown

--- huntduck/Assets/ScorePopup.cs
+++ huntduck/Assets/ScorePopup.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class ScorePopup : MonoBehaviour
 {
     public Vector3 movePopup = new Vector3(0f, 0.1f, 0f);
+    public float moveSpeedFast = 60f;
+    public float moveSpeedSlow = 8f;
     public float scaleAmount = 1f;
 
     public float disappearTimerMax = 1f;
@@ -15,6 +17,13 @@ public class ScorePopup : MonoBehaviour
     private Text scoreText;
     private Color textColor;
 
+    // dropdown to select effect you want
+    public enum EffectType { ScaleAndShrink, ScalePauseFade, FloatAndFade }
+    public EffectType effectType;
+
+    public delegate void SelectedDelegate();
+    SelectedDelegate effectMethod;
+
     void Start()
     {
         //Destroy(gameObject, disappearTimer);
@@ -22,38 +31,116 @@ public class ScorePopup : MonoBehaviour
         scoreText = GetComponentInChildren<Text>();
         textColor = scoreText.color;
         disappearTimer = disappearTimerMax;
+        movePopup *= moveSpeedFast;
+        Debug.Log("movePopup + moveSpeedFast is " + movePopup);
+        RunEffectSelected(effectType);
     }
 
     void Update()
     {
-        // convert options to functions make a dropdown?
-        if (disappearTimer > disappearTimerMax * 5/6)
+        effectMethod();
+    }
+
+    void RunEffectSelected(EffectType _effectType)
+    {
+        switch (effectType)
+        {
+            case EffectType.ScaleAndShrink:
+                effectMethod = ScaleAndShrink;
+                break;
+            case EffectType.ScalePauseFade:
+                effectMethod = ScalePauseFade;
+                break;
+            case EffectType.FloatAndFade:
+                effectMethod = FloatAndFade;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ScaleAndShrink()
+    {
+        transform.position += movePopup * Time.deltaTime;
+        movePopup -= movePopup * moveSpeedSlow * Time.deltaTime;
+        Debug.Log("movePopup - movePopup * moveSpeedSlow is " + movePopup);
+
+        if (disappearTimer > disappearTimerMax / 2)
         {
             // first half of popup lifetime
             transform.localScale += Vector3.one * scaleAmount * Time.deltaTime;
-            transform.position += movePopup * Time.deltaTime;
         }
-        else if (disappearTimer < disappearTimerMax * 1/3)
+        else 
         {
             // second half of popup lifetime
-            //transform.localScale -= Vector3.one * scaleAmount * Time.deltaTime; // make smaller
-
-            //disappear
-            textColor.a -= disappearSpeed * Time.deltaTime;
-            scoreText.color = textColor;
-
+            transform.localScale -= Vector3.one * scaleAmount * Time.deltaTime;
         }
 
         disappearTimer -= Time.deltaTime;
 
-        //if(disappearTimer < 0)
-        //{
-        //    textColor.a -= disappearSpeed * Time.deltaTime;
-        //    scoreText.color = textColor;
-        //}
-        if (textColor.a <= 0)
+        if (disappearTimer < 0)
         {
-            Destroy(gameObject);
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            scoreText.color = textColor;
+            if (textColor.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+    void ScalePauseFade()
+    {
+        //transform.position += movePopup * Time.deltaTime;
+        //movePopup -= movePopup * moveSpeedSlow * Time.deltaTime;
+        //Debug.Log("movePopup - movePopup * moveSpeedSlow is " + movePopup);
+
+        if (disappearTimer > disappearTimerMax / 2)
+        {
+            // scale up, more quickly at first then slower
+            transform.localScale += Vector3.one * scaleAmount * Time.deltaTime;
+            scaleAmount -= scaleAmount * moveSpeedSlow * Time.deltaTime;
+        }
+        //else if (disappearTimer < disappearTimerMax * 2/3 && disappearTimer > disappearTimerMax * 1 / 3)
+        //{
+        //    transform.position += movePopup * Time.deltaTime;
+        //    movePopup -= movePopup * moveSpeedSlow * Time.deltaTime;
+        //}
+        else
+        {
+            // move up
+            transform.position += movePopup * Time.deltaTime;
+
+            // disappear
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            scoreText.color = textColor;
+
+            if (textColor.a <= 0)
+            {
+                // if completely faded, destroy
+                Destroy(gameObject);
+            }
+        }
+
+        disappearTimer -= Time.deltaTime;
+    }
+
+    void FloatAndFade()
+    {
+        //transform.localScale += Vector3.one * scaleAmount * Time.deltaTime;
+        transform.position += movePopup * Time.deltaTime;
+
+        disappearTimer -= Time.deltaTime;
+
+        if (disappearTimer < 0)
+        {
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            scoreText.color = textColor;
+            if (textColor.a <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
