2fc792a39135d8558f564faaa30538549927dfdd
Ben Barone <benjamin.barone1@gmail.com>
2022-06-20 00:03:05 -0400

    added health bar

--- huntduck/Assets/Scripts/Egg.cs
+++ huntduck/Assets/Scripts/Egg.cs
@@ -5,24 +5,24 @@ using huntduck;
 public class Egg : MonoBehaviour
 {
     public float eggSpeed = 2f;
-    public float eggDamage = 34f;
+    public int eggDamage = 1;
     public AudioClip hitPlayerSound;
 
     public bool isHeatSeeking = false; 
     private bool isFrozen = false; 
 
     private BNG.Damageable thisDamageable;
-    private PlayerData playerData;
+    private PlayerData player;
     private Vector3 playerPosition;
     private Vector3 target;
 
-    public delegate void EggHitPlayer();
+    public delegate void EggHitPlayer(int damage);
     public static event EggHitPlayer onEggHitPlayer;
 
     void Awake()
     {
-        playerData = ObjectManager.instance.player;
-        playerPosition = playerData.controller.position;
+        player = ObjectManager.instance.player;
+        playerPosition = player.controller.position;
         thisDamageable = GetComponent<BNG.Damageable>();
         target = playerPosition + (playerPosition - transform.position); // by default, target overshoots player position so egg flies past player
     }
@@ -41,8 +41,7 @@ public class Egg : MonoBehaviour
     {
         if (isHeatSeeking)
         {
-            // when heat seeking, update target each frame to track player
-            target = playerData.controller.position; 
+            target = player.controller.position; 
         }
         if (!isFrozen)
         {
@@ -61,30 +60,33 @@ public class Egg : MonoBehaviour
         if (thisEgg == gameObject)
         {
             thisDamageable.DestroyThis();
-            isFrozen = true; // egg has 2s destroy delay for particle effect, freeze its position once hits something so doesn't keep moving & double hit
+
+            //isFrozen = true; // egg has 2s destroy delay for particle effect, freeze its position once hits something so doesn't keep moving & double hit
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("egg collided with " + other.name);
-
         if (other.tag == TagManager.SHOOTINGSTAND_TAG)
         {
             Destroy(other.gameObject);
         }
         else if (other.tag == TagManager.PLAYER_TAG)
         {
-            playerData.health -= eggDamage;
-            onEggHitPlayer?.Invoke();
-            if (playerData.health > 0)
-            {
-                BNG.VRUtils.Instance.PlaySpatialClipAt(hitPlayerSound, transform.position, 1f, 1f);
-            }
-            //Debug.Log("Did " + eggDamage + " eggDamage to player");
-            //Debug.Log("PlayerHealth is now " + playerData.health);
+            DamagePlayer(eggDamage);
         }
 
         EggSplode(gameObject); // destroy egg any time it hits anything
+    }
+
+    void DamagePlayer(int damage)
+    {
+        onEggHitPlayer?.Invoke(damage);
+
+        // play this "ouch" sound as long as not dead (that's a dif sound)
+        if (player.maxHealth > 0)
+        {
+            BNG.VRUtils.Instance.PlaySpatialClipAt(hitPlayerSound, transform.position, 1f, 1f);
+        }
     }
 }
