0c91e72b283ae2861c9b17f92f7e5d6704988dca
Ben Barone <benjamin.barone1@gmail.com>
2022-07-08 22:40:22 -0400

    Add button that loads new scene
    - set up new button SQUAD UP that takes payer to blank multiplayer scene
    - added DontDestroyMe script to player object, so player persists between scenes
    - cleaned up unused scripts by dumping in MISC folder within Scenes folder

--- huntduck/Assets/Scripts/NextLevel.cs
+++ huntduck/Assets/Scripts/NextLevel.cs
@@ -6,27 +6,27 @@ public class NextLevel : MonoBehaviour
 {
     public string nextLevel;
 
-    public void gotoLevel()
-    {
-     StartCoroutine(GoToLevelAsync(nextLevel));
-     Debug.Log("GTL going to: " + nextLevel);
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
-            Debug.Log("KeyDown.G: Go to next level");
             StartCoroutine(GoToLevelAsync(nextLevel));
             // SceneManager.LoadScene(nextLevel);
+            Debug.Log("KeyDown.G: Go to next level: " + nextLevel);
         }
+    }
+
+    public void gotoLevel()
+    {
+        StartCoroutine(GoToLevelAsync(nextLevel));
+        Debug.Log("Go to next level: " + nextLevel);
     }
 
     IEnumerator GoToLevelAsync(string nextLevel)
     {
 
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextLevel);
-        Debug.Log("GTLA going to: " + nextLevel);
+        Debug.Log("Async going to: " + nextLevel);
 
         while (!asyncLoad.isDone)
         {
