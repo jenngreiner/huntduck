0d946d8c6f33483e7ceb502f1680b27609d1e0de
Ben Barone <benjamin.barone1@gmail.com>
2022-08-31 15:22:35 -0400

    Nametag now being set via RPC - YES

--- huntduck/Assets/Scripts/NetworkPlayerSpawner.cs
+++ huntduck/Assets/Scripts/NetworkPlayerSpawner.cs
@@ -7,47 +7,23 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     public string remotePlayerName;
     private GameObject spawnedPlayer;
+    private NameTag npTag;
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
         spawnedPlayer = PhotonNetwork.Instantiate(remotePlayerName, transform.position, transform.rotation);
         BNG.NetworkPlayer np = spawnedPlayer.GetComponent<BNG.NetworkPlayer>();
-        NameTag npTag = spawnedPlayer.GetComponent<NameTag>();
+        npTag = spawnedPlayer.GetComponent<NameTag>();
 
         if (np)
         {
             np.transform.name = "MyRemotePlayer";
             np.AssignPlayerObjects();
 
-            string playerNickName;
 
-            if (Application.isEditor)
-            {
-                playerNickName = "Player " + PhotonNetwork.LocalPlayer.ActorNumber;
-
-            }
-            else
-            {
-                // check OculusID
-                Debug.Log("Player Oculus ID = " + PlatformManager.s_instance.m_myOculusID);
-
-                playerNickName = PlatformManager.s_instance.m_myOculusID;
-            }
-
+            photonView.RPC("SetNickName", RpcTarget.All);
             
-            PhotonNetwork.NickName = playerNickName;
-            Debug.Log("PhotonNetwork.NickName is " + PhotonNetwork.NickName);
-
-            npTag.nameTag.text = PhotonNetwork.LocalPlayer.NickName;
-            Debug.Log("npTag.nameTag.text is " + npTag.nameTag.text);
-
-
-            // WHAT WE GOT YO
-            Debug.Log("This player's nickname is " + PhotonNetwork.LocalPlayer.NickName);
-            Debug.Log("This player's actorNumber is " + PhotonNetwork.LocalPlayer.ActorNumber);
-            Debug.Log("This player's used ID is " + PhotonNetwork.LocalPlayer.UserId);
-            Debug.Log("'This player is local' is " + PhotonNetwork.LocalPlayer.IsLocal);
             
             // consider seeding scoreboard here
             // player is added to PhotonNetwork.PlayerList
@@ -59,5 +35,37 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
     {
         base.OnLeftRoom();
         PhotonNetwork.Destroy(spawnedPlayer);
+    }
+
+    [PunRPC]
+    void SetNickName()
+    {
+        string playerNickName;
+
+        if (Application.isEditor)
+        {
+            playerNickName = "Player " + PhotonNetwork.LocalPlayer.ActorNumber;
+
+        }
+        else
+        {
+            // check OculusID
+            Debug.Log("Player Oculus ID = " + PlatformManager.s_instance.m_myOculusID);
+
+            playerNickName = PlatformManager.s_instance.m_myOculusID;
+        }
+
+        PhotonNetwork.NickName = playerNickName;
+        Debug.Log("PhotonNetwork.NickName is " + PhotonNetwork.NickName);
+
+        npTag.nameTag.text = PhotonNetwork.NickName;
+        Debug.Log("npTag.nameTag.text is " + npTag.nameTag.text);
+
+
+        //// WHAT WE GOT YO
+        //Debug.Log("This player's nickname is " + PhotonNetwork.LocalPlayer.NickName);
+        //Debug.Log("This player's actorNumber is " + PhotonNetwork.LocalPlayer.ActorNumber);
+        //Debug.Log("This player's used ID is " + PhotonNetwork.LocalPlayer.UserId);
+        //Debug.Log("'This player is local' is " + PhotonNetwork.LocalPlayer.IsLocal);
     }
 }
